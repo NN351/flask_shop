74de8087e812e3be57565b2a56347ccf3aed46fb
NN351 <q7wert1y@gmail.com>
2023-05-22 18:44:16 +0600

    ff

--- src/config/settings.py
+++ src/config/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'src.config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'flask_db',
         'USER': 'flask_user',
         'PASSWORD': 'qwerty123',
