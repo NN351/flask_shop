7aee5af594fffb7257ae3a0c6edbc23d173b21d9
NN351 <q7wert1y@gmail.com>
2023-05-22 20:13:11 +0600

    api updated

--- src/apps/api/urls.py
+++ src/apps/api/urls.py
@@ -1,5 +1,21 @@
 from django.urls import path
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Flask_shop API",
+        default_version='v1',
+        description="FoodTalesApi",
+        contact=openapi.Contact(email="sultanbek9899@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
+
+
 urlpatterns = [
-    
+    path('docs/', schema_view.with_ui("swagger")),
 ]
--- src/apps/product/urls.py
+++ src/apps/product/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
 
-urlpatterns = [
-    
-]   
+from rest_framework.routers import DefaultRouter
+
+from src.apps.product import views
+
+router = DefaultRouter()
+
+router.register('product', views.ProductViewSet, basename="product")
+router.register("category", views.CategoryViewSet, basename="category")
+
+
+urlpatterns = []  
+
+
+urlpatterns += router.urls
+
